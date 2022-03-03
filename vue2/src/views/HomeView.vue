6c51a534e84910a6b3de34bb97c7a8981c12b297
marce-ramirez <marcela.ramirez@thoughtworks.com>
2022-03-03 10:59:31 +0100

    Add Vue3 course

---
+++ vue2/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <AppHeader />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import AppHeader from '@/components/AppHeader.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    AppHeader,
+  },
+};
+</script>
